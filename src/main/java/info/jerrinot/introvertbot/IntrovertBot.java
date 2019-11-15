17da10f428ca4de49d7ea4210a24d9917a9aaec9
jerrinot <jaromir.hamala@gmail.com>
2019-11-15 18:13:16 +0100

    wip: weather

--- src/main/java/info/jerrinot/introvertbot/IntrovertBot.java
+++ src/main/java/info/jerrinot/introvertbot/IntrovertBot.java
@@ -12,12 +12,22 @@ import info.jerrinot.introvertbot.darknet.DarknetSource;
 import okhttp3.OkHttpClient;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Point;
+import org.openweathermap.api.DataWeatherClient;
+import org.openweathermap.api.UrlConnectionDataWeatherClient;
+import org.openweathermap.api.model.currentweather.CurrentWeather;
+import org.openweathermap.api.query.Language;
+import org.openweathermap.api.query.QueryBuilderPicker;
+import org.openweathermap.api.query.ResponseFormat;
+import org.openweathermap.api.query.Type;
+import org.openweathermap.api.query.UnitFormat;
+import org.openweathermap.api.query.currentweather.CurrentWeatherOneLocationQuery;
 
 import static com.hazelcast.jet.aggregate.AggregateOperations.maxBy;
 import static com.hazelcast.jet.contrib.influxdb.InfluxDbSinks.influxDb;
 import static com.hazelcast.jet.impl.util.ExceptionUtil.rethrow;
 import static com.hazelcast.jet.pipeline.WindowDefinition.sliding;
 import static info.jerrinot.introvertbot.Utils.countObjects;
+import static info.jerrinot.introvertbot.Utils.toInfluxDBPoint;
 import static info.jerrinot.introvertbot.darknet.DarknetSource.json2Frame;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -26,6 +36,8 @@ import static org.influxdb.InfluxDB.LogLevel.FULL;
 
 public class IntrovertBot {
 //    private static final String HOST = "10.0.0.61";
+    private static final boolean SEND_TO_INFLUX = false;
+
     private static final String DARKNET_HOST = "192.168.1.111";
     private static final int DARKNET_PORT = 8090;
     private static final String INFLUXDB_PASSWORD_PROPNAME = "introvert.influxdb.password";
@@ -37,9 +49,12 @@ public class IntrovertBot {
     private static final String INFLUXDB_RETENTION_POLICY = "trial";
     private static final int INFLUXDB_TIMEOUT_SECONDS = 60;
 
-    private static final boolean SEND_TO_INFLUX = false;
+    private static final String OPENWEATHER_CITY = "Brno";
+    private static final String OPENWEATHER_COUNTRY_CODE = "CZ";
+    private static final String OPENWEATHER_API_KEY = "TODO";
 
-    public static void main(String[] args) throws Exception {
+
+    public static void nemain(String[] args) throws Exception {
         if (INFLUXDB_PASSWORD == null || INFLUXDB_PASSWORD.isEmpty()) {
             throw new IllegalArgumentException("Property '" + INFLUXDB_PASSWORD_PROPNAME +"' is not set to InfluxDB password");
         }
@@ -52,11 +67,7 @@ public class IntrovertBot {
                 .window(sliding(10_000, 1_000))
                 .aggregate(maxBy(Long::compareTo))
                 .map(WindowResult::result)
-                .map(e -> Point.measurement("people")
-                        .addField("value", e)
-                        .time(System.currentTimeMillis(), MILLISECONDS)
-                        .tag("tag", "count")
-                        .build())
+                .apply(toInfluxDBPoint())
                 .drainTo(createSink());
 
         JetConfig jetConfig = new JetConfig();
@@ -64,6 +75,32 @@ public class IntrovertBot {
         JetInstance jet1 = Jet.newJetInstance(jetConfig);
         Job job = jet1.newJob(pipeline);
         job.join();
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        DataWeatherClient client = new UrlConnectionDataWeatherClient(API_KEY);
+//        CurrentWeatherOneLocationQuery currentWeatherOneLocationQuery = QueryBuilderPicker.pick()
+//                .currentWeather()
+//                .oneLocation()
+//                .byCityName("Brno")
+//                .countryCode("CZ")
+//                .type(Type.ACCURATE)
+//                .language(Language.ENGLISH)
+//                .responseFormat(ResponseFormat.JSON)
+//                .unitFormat(UnitFormat.METRIC)
+//                .build();
+//        CurrentWeather currentWeather = client.getCurrentWeather(currentWeatherOneLocationQuery);
+//        System.out.println((currentWeather));
+//    }
+
+    private static String prettyPrint(CurrentWeather currentWeather) {
+        return String.format(
+                "Current weather in %s(%s):\ntemperature: %.1f ℃\nhumidity: %.1f %%\npressure: %.1f hPa\n",
+                currentWeather.getCityName(), currentWeather.getSystemParameters().getCountry(),
+                currentWeather.getMainParameters().getTemperature(),
+                currentWeather.getMainParameters().getHumidity(),
+                currentWeather.getMainParameters().getPressure()
+        );
     }
 
     private static Sink<Point> createSink() {
--- src/main/java/info/jerrinot/introvertbot/Utils.java
+++ src/main/java/info/jerrinot/introvertbot/Utils.java
@@ -2,10 +2,21 @@ package info.jerrinot.introvertbot;
 
 import com.hazelcast.jet.function.FunctionEx;
 import com.hazelcast.jet.pipeline.StreamStage;
+import org.influxdb.dto.Point;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public final class Utils {
     private Utils() {
 
+    }
+
+    public static FunctionEx<StreamStage<Long>, StreamStage<Point>> toInfluxDBPoint() {
+        return s -> s.map(e -> Point.measurement("people")
+                .addField("value", e)
+                .time(System.currentTimeMillis(), MILLISECONDS)
+                .tag("tag", "count")
+                .build());
     }
 
     public static FunctionEx<StreamStage<Frame>, StreamStage<Long>> countObjects(String objectClass) {
